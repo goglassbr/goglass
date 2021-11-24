72992562f01e6d7e1aec70e53ee3d4e45067d844
Daniel Alves <danalvescc@gmail.com>
2021-11-24 13:36:19 -0300

    Feat(BF): change black friday

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -130,11 +130,11 @@ const Hero = () => {
   return (
     <Wrapper style={{ backgroundImage: `url(${imgs[activeImg]})` }}>
       <BannerText>
-        <h2>TRINCOU A TELA?</h2>
-        <h3>Não deixe para depois!</h3>
+        <h2>BLACK FRIDAY</h2>
+        <h3>25% off na troca do seu vidro!</h3>
         <Line></Line>
         <h4>
-          Economize até 70% na troca do seu vidro sem precisar sair de casa
+          Fale com a gente e conserte sua tela sem gastar quase nada!
         </h4>
         <Button href="#services">QUERO ECONOMIZAR!</Button>
       </BannerText>
